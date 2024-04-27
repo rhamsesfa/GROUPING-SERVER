6c6dc22d28ea786c2f8dc90069bd18a69696df96
Glitch (glitch-hello-node) <none>
2024-04-27 22:40:12 +0000

    ⚓️🛥 Checkpoint ./controllers/User.js:60031220/8565 ./routes/User.js:60031220/46

--- controllers/User.js
+++ controllers/User.js
@@ -1,57 +1,84 @@
-const User = require("../models/User"); 
+const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
+exports.Register = (req, res) => {
+  User.findOne({ phone: req.body.phone }).then(
+    (user) => {
+      if (user) {
+        res.status(201).json({ status: 1, message: "Adresse déjà utilisée" });
+      } else {
+        bcrypt.hash(req.body.password, 10).then(
+          async (hash) => {
+            const newUser = User({
+              email: req.body.email,
+              name: req.body.name,
+              password: hash,
+              date: new Date(),
+            });
+
+            const _id = await newUser.save().then((uss) => {
+              return uss._id;
+            });
+
+            res.status(201).json({
+              status: 0,
+              message: "Utilisateur ajouté avec succès",
+              token: jwt.sign(
+                { userId: _id },
+                "JxqKuulLNPCNfytiyqtsygygfRJYTjgkbhilaebAqetflqRfhhouhpb"
+              ),
+            });
+          },
+          (err) => {
+            res.status(505).json({ err });
+          }
+        );
+      }
+    },
+    (err) => {
+      console.log(err);
+      res.status(500).json({ err });
+    }
+  );
+};
 
 exports.signUp = (req, res) => {
-  
-  
-  User.findOne({phone: req.body.email}).then((user) => {
-    
-    if(user){
-      
-      res.status(201).json({status: 1, message: "Adresse déjà utilisée"}); 
-      
-    }else{
-      
-      bcrypt.hash(req.body.password, 10).then(async (hash) => {
-        
-        const newUser = User({
-          email: req.body.email, 
-          name: req.body.name, 
-          password: hash, 
-          date: new Date()
-        })
-        
-      
-        
-           const us =   await  newUser.save().then((uss) => { return uss._id})
-          
-         
+  User.findOne({ phone: req.body.email }).then(
+    (user) => {
+      if (user) {
+        res.status(201).json({ status: 1, message: "Adresse déjà utilisée" });
+      } else {
+        bcrypt.hash(req.body.password, 10).then(
+          async (hash) => {
+            const newUser = User({
+              email: req.body.email,
+              name: req.body.name,
+              password: hash,
+              date: new Date(),
+            });
 
-            
-  
-                res.status(201).json({status: 0, message: "Utilisateur ajouté avec succès", token: jwt.sign(
-                {userId: us._id}, 
-                 "JxqKuulLNPCNfytiyqtsygygfRJYTjgkbhilaebAqetflqRfhhouhpb"
-                
-            )}); 
-     
-               
-               
- 
-          
-      }, (err) => {
-        
-         res.status(505).json({err});
-          
-      })
-        
+            const _id = await newUser.save().then((uss) => {
+              return uss._id;
+            });
+
+            res.status(201).json({
+              status: 0,
+              message: "Utilisateur ajouté avec succès",
+              token: jwt.sign(
+                { userId: _id },
+                "JxqKuulLNPCNfytiyqtsygygfRJYTjgkbhilaebAqetflqRfhhouhpb"
+              ),
+            });
+          },
+          (err) => {
+            res.status(505).json({ err });
+          }
+        );
+      }
+    },
+    (err) => {
+      res.status(505).json({ err });
     }
-      
-  }, (err) => {
-    
-      res.status(505).json({err})
-  })
-  
-}
+  );
+};
--- routes/User.js
+++ routes/User.js
@@ -8,7 +8,7 @@ const userCtrl = require("../controllers/User");
 
 //router.get("/adduser", userCtrl.SignUp);
 
-
+router.post("/adduser", userCtrl.Register); 
 
 
 

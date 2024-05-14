ce906de2704172c1cef434644e6437323d90dd7b
Glitch (glitch-hello-node) <none>
2024-05-14 23:36:02 +0000

    🙌🚩 Checkpoint ./models/Announcement.js:60031220/454 ./controllers/Announcement.js:60031220/175

---
+++ controllers/Announcement.js
@@ -0,0 +1,9 @@
+const Announcement = require("../models/Announcement"); 
+
+
+exports.addAnnouncement = (req, res) => {
+  
+    const announcement = new Announcement({
+        
+    })
+}
--- models/Announcement.js
+++ models/Announcement.js
@@ -2,5 +2,18 @@ const mongoose = require("mongoose");
 
 const AnnouncementSchema = mongoose.Schema({
     
-    
+    startCity: {type: String}, 
+    endCity: {type: String}, 
+    dateOfDeparture: {type: Date}, 
+    kilosCount: {type: Number}, 
+    kiloPrice: {type : Number}, 
+    company: {type: String}, 
+    description: {type: String}, 
+    userId: {type: String}, 
+    status: {type: String}, 
+    date: {type: Date}, 
+    active: {type: Boolean}
 })
+
+
+module.exports = mongoose.model("Announcement", AnnouncementSchema); 

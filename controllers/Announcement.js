966100bae642e59c1417b920dbe603273c0c896e
Glitch (glitch-hello-node) <none>
2024-05-15 08:08:08 +0000

    🎱🌘 Checkpoint ./controllers/Announcement.js:60031220/90

--- controllers/Announcement.js
+++ controllers/Announcement.js
@@ -4,6 +4,8 @@ const Announcement = require("../models/Announcement");
 exports.addAnnouncement = (req, res) => {
   
     const announcement = new Announcement({
-        
+        startCity: req.body.startCity, 
+        endCity: req.body.endCity, 
+        dateOfDe
     })
 }

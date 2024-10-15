84c66a403827266592b84df52ea175a1c69ffa36
Glitch (glitch-hello-node) <none>
2024-10-15 09:36:56 +0000

    🌮🍰 Checkpoint ./controllers/Message.js:60031220/1320

--- controllers/Message.js
+++ controllers/Message.js
@@ -27,6 +27,30 @@ exports.getMessages = async (req, res) => {
 
 }
 
+exports.getMessagesById = async (req, res) => {
+  
+  //console.log(req.body);
+  
+  try{
+    
+    const messages = await  Message.find({user1Id: req.auth.userId}).sort({date: -1}).skip(req.body.startAt).limit(10)
+    
+    const user = await User.findOne({_id: req.body.user2});
+    
+    const count = await Announcement.countDocuments({userId: req.body.user2, active: true})
+    
+    res.status(200).json({status: 0, messages, user, startAt: messages.length === 10 ? parseInt(req.body.startAt) + 10 : null, count})
+    
+    
+    
+  }catch(e){
+    
+      console.log(e); 
+      res.status(505).json({e})
+  }
+  
+
+}
 
 exports.addMessage = async (req, res) => {
     

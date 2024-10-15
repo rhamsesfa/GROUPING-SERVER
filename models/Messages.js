3714f39be06d496cb83664d104533e5f5a84ffe9
Glitch (glitch-hello-node) <none>
2024-10-15 09:02:49 +0000

    💨💪 Checkpoint ./models/Messages.js:60031220/612

--- models/Messages.js
+++ models/Messages.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose'); 
 
-
 const messageSchema = mongoose.Schema({
-    
-    text: {type: String}, 
-    date: {type: Date}, 
-    user1Id: {type: String}, 
-    user2Id: {type: String}
-  
+    text: { type: String }, 
+    date: { type: Date }, 
+    user1Id: { type: String }, 
+    user2Id: { type: String }, 
+    id_annonce: { type: String }  // champ facultatif ajouté
 })
-
 
 module.exports = mongoose.model("Message", messageSchema);

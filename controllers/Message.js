35352a35d91882baeda87b4d5525d610b5fdb19c
Glitch (glitch-hello-node) <none>
2024-10-23 11:40:48 +0000

    🌈🌥 Checkpoint ./controllers/Message.js:60031220/39

--- controllers/Message.js
+++ controllers/Message.js
@@ -113,7 +113,7 @@ exports.getMessagesById = async (req, res) => {
 };
 
 exports.addMessage = async (req, res) => {
-  console.log(req.body);
+  console.log(req.body, 'envoi du message en base de données');
   const newMessage = new Message({
     date: new Date(),
     text: req.body.text,

445720e88c37ed220e39106563d1b60f73238ea2
Glitch (glitch-hello-node) <none>
2024-10-24 23:01:36 +0000

    🚏🎄 Checkpoint ./controllers/City.js:60031220/443 ./models/City.js:60031220/33

--- controllers/City.js
+++ controllers/City.js
@@ -6,23 +6,26 @@ exports.addCity = (req, res) => {
       
       if(ville){
         
+        res.status(200).json({status: 1, ville});
           
       }else{
         
-      const city = new City({
-      name: req.body.name, 
-      country: req.body.country, 
-      code: req.body.code
-      })
-    
-    city.save().then(() => {
-      
-        res.status(201).json({status: 0})
-    
-    }, (err) => {
-      
-        res.status(505).json({err})
-    })
+          const city = new City({
+          name: req.body.name, 
+          country: req.body.country, 
+          code: req.body.code, 
+          country_id: req.body.country
+            
+          })
+
+        city.save().then(() => {
+
+            res.status(201).json({status: 0});
+
+        }, (err) => {
+
+            res.status(505).json({err})
+        })
       }
       
     }, (err) => {
--- models/City.js
+++ models/City.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const citySchema = mongoose.Schema({
     name: {type: String}, 
     country: {type: String}, 
-    code: {type: String}
+    code: {type: String}, 
+    country_id: {type: String}
 })
 
 module.exports = mongoose.model("City", citySchema);

ca79ed465b1382bbfca1d37a3504cc125de7a3fa
Glitch (glitch-hello-node) <none>
2025-01-13 10:49:48 +0000

    🐑☂️ Checkpoint ./controllers/Notification.js:60031220/445

--- controllers/Notification.js
+++ controllers/Notification.js
@@ -1,7 +1,19 @@
 const Notification = require("../models/Notification"); 
 
 
-exports.readNotifications = (req, res) => {
+exports.readNotifications = async (req, res) => {
+  
+  try{
+      
+    await Notification.updateMany({receiverId: req.auth.userId}, {$set: {view: true}}); 
+    
+    res.status(200).json({status: 0})
+    
+  }catch(err){
+    
+    console.log(err); 
+    res.status(505).json({err})
+  }
   
     
 }

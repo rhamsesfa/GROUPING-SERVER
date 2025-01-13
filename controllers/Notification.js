0320403db5bee22464fbc9859739bb9a2b9c2034
Glitch (glitch-hello-node) <none>
2025-01-13 15:52:05 +0000

    🌍🍞 Checkpoint ./controllers/Notification.js:60031220/767

--- controllers/Notification.js
+++ controllers/Notification.js
@@ -1,5 +1,5 @@
 const Notification = require("../models/Notification"); 
-
+const Message = require("../models/Messages");
 
 exports.viewNotifications = async (req, res) => {
   
@@ -33,4 +33,25 @@ exports.getNotReadNotifications = async(req, res) => {
         console.log(err); 
         res.status(505).json({err})
     }
+}
+
+exports.getNotifications = async (req, res) => {
+  
+  const startAt = req.body.startAt ? req.body.startAt : 0;
+  
+    try{
+        
+      const notifs = await  Notification.find({receiverId: req.auth.userId, authorId: "grouping"}).sort({date: -1}).limit(3);
+      
+      const messages = await Message.find({$or: [{user1Id: req.auth.userId}, {user2Id: req.auth.userId }]}).sort({date: -1}).skip(startAt).limit(10); 
+      
+      
+      res.status(200).json({}); 
+      
+    }catch(err){
+      
+        console.log(err); 
+        res.status(505).json({err})
+    }
+   
 }

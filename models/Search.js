3443962bc51012dd8c732e8f40c575dad1cba668
Glitch (glitch-hello-node) <none>
2025-01-13 13:09:30 +0000

    🌻🚧 Checkpoint ./models/Search.js:60031220/330

--- models/Search.js
+++ models/Search.js
@@ -1,2 +1,15 @@
 const mongoose = require("mongoose");
 
+const searchSchema = mongoose.Schema({
+  
+     startCity : {type: String}, 
+     endCity: {type: String}, 
+     month: {type: String}, 
+     year: {type: String}, 
+     type: {type: Boolean}, 
+     userId: {type: String}
+  
+    
+})
+
+module.exports = mongoose.model("Search", searchSchema)
